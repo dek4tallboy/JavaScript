5c7237e2527eb5ecbf3c9951e65f05634521a4c0
dek4tallboy <andre3klima@gmail.com>
2026-03-26 18:18:39 -0300

    removendo arquivos desnecessários

--- js.js
+++
@@ -1,9 +0,0 @@
-alert("Esse é o alerta(alert) e é a primeira forma de escrever na tela");
-
-document.getElementById("demo").innerHTML = "Esse é o innerHTML e é a 2° forma de escrever na tela";
-
-document.write("Esse é o write e é a 3° forma de escrever na tela");
-
-console.log("Esse é um tipo de alerta silencioso, não aparece na tela, mas se você utilizar o f12 e ir em console conseguirá vê-lo");
-
-//Isso é um comentário em JS
